ac6b1079d43349895317577693ca729ed9cfc490
Bhuviofficial <bhuvi1928.g@gmail.com>
2025-11-02 13:55:09 +0530

    changes updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import ProductsPage from "./Pages/ProductsPage";
 import CartPage from "./Pages/CartPage";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   const [cart, setCart] = useState([]);
